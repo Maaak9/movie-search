7d3325939e410473b25daaf31bf707c525bec07b
Maaak9 <marcuskarrm@gmail.com>
2024-04-13 10:31:39 +0200

    Added MovieGrid component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import React, { useEffect } from 'react';
 import './App.scss';
-import MovieCard from './components/MovieCard/MovieCard';
 import MovieSearch from './components/MovieSearch/MovieSearch';
+import MovieGrid from './components/MovieGrid/MovieGrid';
 
 function App() {
-
-  const movie = {
-    "Title": "Harry Potter and the Deathly Hallows: Part 2",
-    "Year": "2011",
-    "imdbID": "tt1201607",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BMGVmMWNiMDktYjQ0Mi00MWIxLTk0N2UtN2ZlYTdkN2IzNDNlXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg"
-  }
 
   return (
     <div className="App">
       <h1>hello</h1>
       <MovieSearch />
-      <div style={{ width: "300px" }}>
-        <MovieCard movie={movie} />
-      </div>
+      <MovieGrid />
     </div>
   );
 }
---
+++ src/components/MovieGrid/MovieGrid.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import MovieGrid from './MovieGrid';
+
+
+test('Renders MovieGrid component', () => {
+  render(<MovieGrid />);
+
+  // TODO fix test
+  expect(1).toBe(1)
+ 
+});
