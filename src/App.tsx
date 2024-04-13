36ad885ef0c35bb6bc571c615b176c8222068c0a
Maaak9 <marcuskarrm@gmail.com>
2024-04-13 10:01:03 +0200

    WIP tested the API and added types

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.scss';
+import { fetchMovie, fetchMovies } from './api/api';
 
 function App() {
+
+  useEffect(() => {
+    fetchMovies();
+    fetchMovie();
+  }, [])
+
   return (
     <div className="App">
       <h1>hello</h1>
---
+++ src/api/api.ts
@@ -0,0 +1,23 @@
+
+const API_URL = 'http://www.omdbapi.com/?apikey=2356a083';
+
+// https://www.omdbapi.com/
+
+export const fetchMovies = () => {
+  const searchTerm = 'Harry'
+  fetch(`${API_URL}&s=${searchTerm}`).then((res) => {
+    res.json().then((json) => {
+        console.log('Movies', json);
+    })
+  })
+}
+
+export const fetchMovie = () => {
+  const movieId = 'tt1201607';
+  fetch(`${API_URL}&i=${movieId}`).then((res) => {
+    res.json().then((json) => {
+        console.log('Movie', json);
+    })
+  })
+
+}
