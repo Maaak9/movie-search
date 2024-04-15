2bf625d75066a0999bf6f4a3659b7214717f7817
Maaak9 <marcuskarrm@gmail.com>
2024-04-15 09:13:46 +0200

    Added test for MovieSearch

--- src/components/MovieSearch/MovieSearch.test.tsx
+++ src/components/MovieSearch/MovieSearch.test.tsx
@@ -1,10 +1,19 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import MovieSearch from './MovieSearch';
+import { MemoryRouter } from 'react-router-dom';
 
 test('Renders MovieSearch component', () => {
-  render(<MovieSearch/>);
 
-  // TODO fix test
-  expect(1).toBe(1)
- 
+  render(
+    <MemoryRouter>
+      <MovieSearch />
+    </MemoryRouter>
+  );
+
+  const searchElement = screen.getByPlaceholderText('Search');
+  expect(searchElement).toBeInTheDocument();
+
+  fireEvent.change(searchElement, { target: { value: 'Harry' }});
+  expect(screen.getByDisplayValue('Harry')).toBeInTheDocument();
+
 });
