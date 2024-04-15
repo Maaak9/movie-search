b93fa4e70723388842a624dab4d76fedc8518ad9
Maaak9 <marcuskarrm@gmail.com>
2024-04-15 08:52:55 +0200

    Fixed test, react router Link in MovieCard required a Provider

--- src/components/MovieCard/MovieCard.test.tsx
+++ src/components/MovieCard/MovieCard.test.tsx
@@ -1,16 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import MovieCard from './MovieCard';
+import { MemoryRouter } from 'react-router-dom';
 
 test('Renders MovieCard component', () => {
   render(
-    <MovieCard movie={{
-      "Title": "Harry Potter and the Deathly Hallows: Part 2",
-      "Year": "2011",
-      "imdbID": "tt1201607",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMGVmMWNiMDktYjQ0Mi00MWIxLTk0N2UtN2ZlYTdkN2IzNDNlXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg"
-    }}
-  />);
+    <MemoryRouter>
+      <MovieCard movie={{
+        "Title": "Harry Potter and the Deathly Hallows: Part 2",
+        "Year": "2011",
+        "imdbID": "tt1201607",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BMGVmMWNiMDktYjQ0Mi00MWIxLTk0N2UtN2ZlYTdkN2IzNDNlXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg"
+      }}
+      />
+    </MemoryRouter>
+    );
   const titleElement = screen.getByText(/Harry Potter and the Deathly Hallows: Part 2/i);
   expect(titleElement).toBeInTheDocument();
 
